d64f7c5a33e3e2ae8f05beb7a30675ac4ec5a546
James Hsia <xw900812@gmail.com>
2015-05-14 15:19:34 -0400

    Method_Print_Sentence

---
+++ Method_Printout_Sentence.py
@@ -0,0 +1,19 @@
+def Colleges():
+    print "Adelphi"
+    print "Pace University"
+    print "NYU"
+
+def Blank():
+    print "--------"
+
+def Highschools():
+    print "Adelphi Academy"
+    print "Madison"
+    print "Lincoln HighSchool"
+
+def main():
+    Highschools()
+    Blank()
+    Colleges()
+
+main()
