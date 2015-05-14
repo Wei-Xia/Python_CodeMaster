7b00e89b9b85abc0c59c7354750064243b42a5b8
James Hsia <xw900812@gmail.com>
2015-05-14 15:17:58 -0400

    Sum with two input numbers

---
+++ Hello_Sum.py
@@ -0,0 +1,13 @@
+print "Hello World"  #python 2.0
+
+print ("Hello World")  #python 3.0
+
+a = input("What is the first integer that you want: ")   #This is asking the user for the first integer
+
+b = input("What is the second integer that you want: ")   #This is asking the user for the second integer
+
+c = b + a
+
+print c  #This is will show the sum of the two integers that we ask user
+
+print "The sum of the Addition input is: %s" % (c)
