2fbf74d33cf7c8b45187ca2e03507592ddb05986
James Hsia <xw900812@gmail.com>
2015-05-14 15:19:19 -0400

    Method_Print

---
+++ Method_Print_Out.py
@@ -0,0 +1,11 @@
+class Mystuff(object):
+
+    def __init__(self):
+        self.Microsoft = "I like Xbox"
+
+    def Sony (self):
+        print "I love PlayStation"
+
+thing = Mystuff()
+thing.Sony()
+print thing.Microsoft
