9f3c19e514b05648af08a495d0cd31f4d3be3919
James Hsia <xw900812@gmail.com>
2015-05-14 15:19:02 -0400

    Print_NameAgeSchool

---
+++ Name_Age_School.py
@@ -0,0 +1,7 @@
+name = 'James'
+age = 27
+school = 'Pace'
+myheight = 6
+myweight = 175
+
+print "My name %r, I'm %r years old, I go to school at %r, and my height is %r" % (name, age, school, myheight)
