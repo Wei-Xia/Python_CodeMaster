0ecd749fb395535c7ee8b25f5d26f78fba3c712a
James Hsia <xw900812@gmail.com>
2015-05-14 15:18:28 -0400

    Sort_Array_With_Input

---
+++ Input_Array.py
@@ -0,0 +1,9 @@
+a = input("What is the first integer that you want in that array: ")
+
+Number = [a, 44, 11, 88, 22, 99, 100]
+
+Number.sort()
+
+print Number
+
+print "The sorted array is: %r" %(Number)
